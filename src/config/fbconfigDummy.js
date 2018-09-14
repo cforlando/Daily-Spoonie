f1b74c61c700c7dafba316188f974e8653e119cc
jmash <jaredmashcraft@gmail.com>
2018-09-13 23:09:37 -0400

    Sets up firebase config

---
+++ src/config/fbconfigDummy.js
@@ -0,0 +1,13 @@
+// not for wayward eyes! <_< You'll need the TOP SECRET
+// config information to get access to THIS database!
+
+const config = {
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: ""
+  };
+
+  export default config;
