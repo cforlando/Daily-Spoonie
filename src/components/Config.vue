3ef4fc8e38d4adc3d94ac78c6a67d97f44854dee
jmash <jaredmashcraft@gmail.com>
2019-01-04 11:38:46 -0500

    :lipstick: Adds Config component to UI

---
+++ src/components/Config.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <b-dropdown variant="link" no-caret>
+            <template slot="button-content">
+                <i class="fas fa-cogs" />
+            </template>
+            <b-dropdown-item>Add Spoons</b-dropdown-item>
+            <b-dropdown-divider />
+            <b-dropdown-item>Log Out</b-dropdown-item>
+        </b-dropdown>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Config",
+        components: {
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
