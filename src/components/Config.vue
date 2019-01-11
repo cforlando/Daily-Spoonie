e71663fbf75f0b72d3617fa0eb06753c12d429ad
jmash <jaredmashcraft@gmail.com>
2019-01-11 14:39:51 -0500

    :sparkles: Adds log out functionality to config dropdown list

--- src/components/Config.vue
+++ src/components/Config.vue
@@ -6,7 +6,7 @@
             </template>
             <b-dropdown-item>Add Spoons</b-dropdown-item>
             <b-dropdown-divider />
-            <b-dropdown-item>Log Out</b-dropdown-item>
+            <b-dropdown-item @click="logout">Log Out</b-dropdown-item>
         </b-dropdown>
     </div>
 </template>
@@ -15,6 +15,11 @@
     export default {
         name: "Config",
         components: {
+        },
+        methods: {
+            logout: function() {
+                this.$store.dispatch("LOG_OUT");
+            }
         }
     }
 </script>
